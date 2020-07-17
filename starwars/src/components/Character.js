85677e81db42982cc4af18bab6180c294f53e01b
JabinWashburn <jabinwashburn@gmail.com>
2020-07-17 09:18:47 -0700

    got a few things to render to the page. need to style and look into getting a couple fields i want rendered

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    
+`
+
+
+
+const Character = props => {
+    return(
+        <Container>
+            <p>Name: {props.name}</p>
+            <p>Birth: {props.birth}</p>
+            <p>Mass: {props.mass}</p>
+        </Container>
+    )
+}
+
+export default Character
